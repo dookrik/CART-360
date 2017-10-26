7dc61ca12342cc04ed304c0421d792f24591a54d
kathleen <katboo96@hotmail.com>
2017-10-26 18:40:06 -0400

    Add files via upload

---
+++ Assignment_1/ASSIGNMENT_1_SENSORS/ASSIGNMENT_1_SENSORS.ino
@@ -0,0 +1,172 @@
+/* This code is used to give life and sensitivity to the octopus... do not squeeze it too hard Qa Q !!! it will wiggle out in pain.
+ *  takes in analog input from a velostat pressure sensor and uses it to measure when a press of a certain threshold is met and reacts accordingly
+
+*/
+// separate code from other arduino
+#include <Servo.h>
+#define SENSOR_PIN_1 A0
+#define SENSOR_PIN_2 A1
+#define SENSOR_PIN_3 A2
+#define SENSOR_PIN_4 A3
+
+Servo servo1;   
+Servo servo2;   
+Servo servo3;   
+Servo servo4;   
+
+#define SERVO_PIN_1 2
+#define SERVO_PIN_2 3
+#define SERVO_PIN_3 4
+#define SERVO_PIN_4 5
+
+// medium treshold == ok for octopus
+int thresholdOk = 400;
+// high value == pain for octopus
+int thresholdOuch = 900;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+ pinMode(3, INPUT);     //force sensor value input
+ servo1.attach(SERVO_PIN_1);
+ servo2.attach(SERVO_PIN_2);
+ servo3.attach(SERVO_PIN_3);
+ servo4.attach(SERVO_PIN_4);
+  
+}
+
+void loop() {
+  
+ //these read the values from our 
+   int sensorPin_1 = analogRead(A0);
+   int sensorPin_2 = analogRead(A1);
+   int sensorPin_3 = analogRead(A2);
+   int sensorPin_4 = analogRead(A3);
+// 1st sensor ===========================================================================================================================
+
+ //if the values from the sensor are higher or equal to the acceptable threshold(soft) vibrate for as long as it has this condition 
+ if(sensorPin_1 >= thresholdOk) {
+    for(int i = 0; i < 50; i++){
+      servo1.write(i);
+      delay(15);  
+      Serial.println(sensorPin_1); 
+   }
+    //reverse action of prev statement when false and stay at 0 deg.
+  } else if(sensorPin_1 < thresholdOk) {
+    for(int i = 50; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+  }
+//-=-=-= higher threshold -=-=-=-=-=-= short sweeps constant for as long as it is being pressed
+  if(sensorPin_1 >= thresholdOuch) {
+    for(int i = 0; i < 5; i++){
+      servo1.write(i);
+      delay(15);
+    }
+   // this returns false which means either under thresholdOk or under thresholdOuch 
+   // this is a problem because  if its not == to 5, it will decrement from a higher sweep value and only decrement by 5
+ } else if(sensorPin_1 < thresholdOuch) {
+    for(int i = 5; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+   }
+   ////-=-=--=-= 2nd sensor-========================================================================================================
+
+   
+ //if the values from the sensor are higher or equal to the acceptable threshold(soft) vibrate for as long as it has this condition 
+ if(sensorPin_2 >= thresholdOk) {
+    for(int i = 0; i < 50; i++){
+      servo1.write(i);
+      delay(15);  
+     // Serial.println(sensorPin_2); 
+   }
+    //reverse action of prev statement when false and stay at 0 deg.
+  } else if(sensorPin_2 < thresholdOk) {
+    for(int i = 50; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+  }
+//-=-=-= higher threshold -=-=-=-=-=-= short sweeps constant for as long as it is being pressed
+  if(sensorPin_2 >= thresholdOuch) {
+    for(int i = 0; i < 5; i++){
+      servo1.write(i);
+      delay(15);
+    }
+   // this returns false which means either under thresholdOk or under thresholdOuch 
+   // this is a problem because  if its not == to 5, it will decrement from a higher sweep value and only decrement by 5
+ } else if(sensorPin_2 < thresholdOuch) {
+    for(int i = 5; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+   }
+   // 3rd sensor===================================================================================================================
+   
+    //if the values from the sensor are higher or equal to the acceptable threshold(soft) vibrate for as long as it has this condition 
+ if(sensorPin_3 >= thresholdOk) {
+    for(int i = 0; i < 50; i++){
+      servo1.write(i);
+      delay(15);  
+     // Serial.println(sensorPin_3); 
+   }
+    //reverse action of prev statement when false and stay at 0 deg.
+  } else if(sensorPin_3 < thresholdOk) {
+    for(int i = 50; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+  }
+//-=-=-= higher threshold -=-=-=-=-=-= short sweeps constant for as long as it is being pressed
+  if(sensorPin_3 >= thresholdOuch) {
+    for(int i = 0; i < 5; i++){
+      servo1.write(i);
+      delay(15);
+    }
+   // this returns false which means either under thresholdOk or under thresholdOuch 
+   // this is a problem because  if its not == to 5, it will decrement from a higher sweep value and only decrement by 5
+ } else if(sensorPin_3 < thresholdOuch) {
+    for(int i = 5; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+   }
+   // 4th sensor =======================================================================================================================
+   
+    //if the values from the sensor are higher or equal to the acceptable threshold(soft) vibrate for as long as it has this condition 
+ if(sensorPin_4 >= thresholdOk) {
+    for(int i = 0; i < 50; i++){
+      servo1.write(i);
+      delay(15);  
+      //Serial.println(sensorPin_4); 
+   }
+    //reverse action of prev statement when false and stay at 0 deg.
+  } else if(sensorPin_4 < thresholdOk) {
+    for(int i = 50; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+  }
+//-=-=-= higher threshold -=-=-=-=-=-= short sweeps constant for as long as it is being pressed
+  if(sensorPin_4 >= thresholdOuch) {
+    for(int i = 0; i < 5; i++){
+      servo1.write(i);
+      delay(15);
+    }
+   // this returns false which means either under thresholdOk or under thresholdOuch 
+   // this is a problem because  if its not == to 5, it will decrement from a higher sweep value and only decrement by 5
+ } else if(sensorPin_4 < thresholdOuch) {
+    for(int i = 5; i <= 0; i--){
+      servo1.write(i);
+      delay(15);
+    }
+   } 
+
+  // loop bracket dont remove > A <
+ }
+
+
+
---
+++ Assignment_1/Assignment_1_limbs.ino
@@ -0,0 +1,167 @@
+/*
+  code for riping off limbs and getting a response!
+  the point of this code is to notify a pin when a circuit is broken and which will result in the rumble
+  of a servo.
+*/
+
+//library
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+
+#define SERVO_PIN_2 2
+#define SERVO_PIN_3 3
+#define SERVO_PIN_4 4
+#define SERVO_PIN_5 5
+
+//to test the current flow
+//int ledPin = 13;
+//byte leds = 0;
+// think of these as temp switches allowing power to flow only in one way
+// switches that act as legs
+int btnAservoP5 = 6;
+int btnBservoP2 = 7;
+//int btnCservoP3 = 8;
+//int btnDservoP4 = 9;
+
+void setup()
+{
+  Serial.begin(9600);
+  //power my switches (legs) through the limbs so it does the opposite  of a nnormal switch
+  pinMode(ledPin, OUTPUT);
+  pinMode(btnAservoP5, INPUT_PULLUP);
+  pinMode(btnBservoP2, INPUT_PULLUP);
+  pinMode(btnCservoP3, INPUT_PULLUP);
+  pinMode(btnDservoP4, INPUT_PULLUP);
+
+  // references for servos
+  servo1.attach(SERVO_PIN_5); // servo 1
+  servo2.attach(SERVO_PIN_2); // servo 2
+  servo3.attach(SERVO_PIN_3); //servo 3
+  servo4.attach(SERVO_PIN_4); //servo 4
+  /*
+    // test that everything is running
+    for (int i = 0; i < 100; i++) {
+     servo2.write(i);
+      servo1.write(i);
+
+     delay(50);
+    }
+
+    digitalWrite(ledPin,HIGH);
+    delay(1000);
+    digitalWrite(ledPin,LOW);
+  */
+}
+
+void loop()
+{
+  // A button. when disconected act=============================================================
+// servo 1 is actually refered to pin 5 !!!!!!!!!!!!!!!
+  if (digitalRead(btnAservoP5) == HIGH)
+  {
+    //digitalWrite(ledPin, HIGH);
+    //loop through and wiggle!
+    for (int i = 0; i < 60; i++)
+    {
+      //this is on =========================================== pin 3
+      servo1.write(i);
+      delay(15);
+    }
+
+  }
+  // when connect dont act, go back to original position and stay at 0 deg..
+  if (digitalRead(btnAservoP5) == LOW)
+  {
+   // digitalWrite(ledPin, LOW);
+    for (int i = 60; i <= 0; i--)
+    {
+      servo1.write(i);
+      delay(15);
+    }
+    digitalWrite(ledPin, LOW);
+    // delay(1000);
+  }
+  //check data
+  //   Serial.println(SERVO_PIN_5);
+
+  // B button connected to servo 2===================================================
+  Serial.println(digitalRead(btnBservoP2));
+  if (digitalRead(btnBservoP2) == HIGH)
+  {
+    //loop through and wiggle!
+    for (int i = 0; i < 60; i++)
+    {
+      //this is on pin 2
+      servo2.write(i);
+      delay(15);
+    }
+
+  }
+  // when connect dont act, go back to original position and stay at 0 deg..
+  if (digitalRead(btnBservoP2) == LOW)
+  {
+
+    for (int i = 60; i <= 0; i--)
+    {
+      servo2.write(i);
+      delay(15);
+    }
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+  }
+
+  // C button connected to servo 1 ===================================================
+  /*
+    if (digitalRead(btnCservoP3) == HIGH)
+    {
+      //loop through and wiggle!
+      for (int i = 0; i < 100; i++)
+      {
+        //this is on pin 5
+        servo3.write(i);
+        delay(15);
+      }
+    }
+    // when connect dont act, go back to original position and stay at 0 deg..
+    if (digitalRead(btnCservoP3) == LOW)
+    {
+
+      for (int i = 100; i <= 0; i--)
+      {
+        servo3.write(i);
+        delay(15);
+      }
+      digitalWrite(ledPin, LOW);
+      delay(1000);
+    }
+
+    //D button and servo 4 ==============================================================
+
+     if (digitalRead(btnDservoP4) == HIGH)
+    {
+      //loop through and wiggle!
+      for (int i = 0; i < 100; i++)
+      {
+        //this is on====================== servo pin 4
+        servo4.write(i);
+        delay(15);
+      }
+    }
+    // when connect dont act, go back to original position and stay at 0 deg..
+    if (digitalRead(btnDservoP4) == LOW)
+    {
+
+      for (int i = 100; i <= 0; i--)
+      {
+        servo4.write(i);
+        delay(15);
+      }
+    }
+  */
+}
+
+
